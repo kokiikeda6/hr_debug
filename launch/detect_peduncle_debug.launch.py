dfe7d9e17cb5ac903d01dfc8579ab425390a8ffd
kokiikeda6 <hikoukikoki@gmail.com>
2026-05-14 19:14:44 +0900

    initial commit

---
+++ launch/detect_peduncle_debug.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    rviz_config = PathJoinSubstitution(
+        [FindPackageShare("hr_debug"), "rviz", "detect_peduncle_debug.rviz"]
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config],
+        parameters=[{"use_sim_time": use_sim_time}],
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="false",
+                description="Use simulation clock if true",
+            ),
+            rviz_node,
+        ]
+    )
