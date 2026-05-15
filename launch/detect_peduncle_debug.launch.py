90e0c6ff65d43943c6e95dbac44ffa9a88aa77b1
kokiikeda6 <hikoukikoki@gmail.com>
2026-05-15 16:54:23 +0900

    fix rviz config

--- launch/detect_peduncle_debug.launch.py
+++ launch/detect_peduncle_debug.launch.py
@@ -19,6 +19,7 @@ def generate_launch_description():
         name="rviz2",
         arguments=["-d", rviz_config],
         parameters=[{"use_sim_time": use_sim_time}],
+        additional_env={"LIBGL_ALWAYS_SOFTWARE": "true"},
         output="screen",
     )
 
